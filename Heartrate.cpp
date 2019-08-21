91bffe35fb8972e401c56de2f3077456a461ea23
Saurabh Sinha <51476953+PrinceSs6312@users.noreply.github.com>
2019-08-21 15:00:05 +0530

    Create Heartrate.cpp

---
+++ Heartrate.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream> 
+#include"Heart.h"
+using namespace std;
+
+void Heartrate()
+{
+int a[10], i;
+int sum = 0;
+printf("\nThe following questions make up the standardised test result.");
+printf("\nThese are your options for each question: ");
+printf("\n");
+printf("\n\t\t1) Not at all.");
+printf("\n\t\t2) Only slightly.");
+printf("\n\t\t3) Partly.");
+printf("\n\t\t4) A lot.");
+printf("\n\t\t5) Quite a lot.");
+printf("\n\t\t6) To a great extent.");
+printf("\n");
+printf("\nFor each of the following question, enter a number corresponding to the above list."); 
+printf("\n1) I do everywork slowly. ");
+scanf("%d", &a[0]);
+printf("\n2) I do exercises regularly. ");
+scanf("%d", &a[1]);
+printf("\n3) I breathe normally while working. ");
+scanf("%d", &a[2]);
+printf("\n4) I have sometime fear and depression. ");
+scanf("%d", &a[3]);
+printf("\n5) I find it hard to make decisions. ");
+scanf("%d", &a[4]);
+printf("\n6) My sleep is disturbed: too little, too much, or disturbed sleep. ");
+scanf("%d", &a[5]);
+printf("\n7) I have lost or gained weight. ");
+scanf("%d", &a[6]);
+
+for(i=0;i<10;i++) 
+{
+    sum = sum + a[i] - 1; 
+}
+
+/*
+if(sum>=70 && sum<80)
+{
+    printf("\n\Heartrate unlikely to happen.");
+}
+else if(sum>=80 && sum<90)
+{
+    printf("\n\nPossible minor Heartrate Risk.");
+}
+else if(sum>=90)
+{
+    printf("\n\nOn the verge of Heartrate Risk.");
+}
+
+else if(sum>100)
+{
+    printf("\n\nSevere Heartrate Risk.");
+}
+*/
+
+printf("\n");
+
+fout<<sum;
+fout<<"\n";
+
+fout.close();
+
+};
