4bf7e2ee9e055ec76e422ca9b80aeb90308ae67e
Saurabh Sinha <51476953+PrinceSs6312@users.noreply.github.com>
2019-08-21 15:13:55 +0530

    Update Heartrate.cpp

--- Heartrate.cpp
+++ Heartrate.cpp
@@ -8,15 +8,14 @@ void Heartrate()
 {
 int a[10], i;
 int sum = 0;
+cin>>heartrate;
 printf("\nThe following questions make up the standardised test result.");
 printf("\nThese are your options for each question: ");
 printf("\n");
-printf("\n\t\t1) Not at all.");
-printf("\n\t\t2) Only slightly.");
-printf("\n\t\t3) Partly.");
-printf("\n\t\t4) A lot.");
-printf("\n\t\t5) Quite a lot.");
-printf("\n\t\t6) To a great extent.");
+printf("\n\t\t1) "7" Not at all.");
+printf("\n\t\t2) "8" Partly.");
+printf("\n\t\t3) "9" A lot.");
+printf("\n\t\t4) "10" To a great extent.");
 printf("\n");
 printf("\nFor each of the following question, enter a number corresponding to the above list."); 
 printf("\n1) I do everywork slowly. ");
@@ -39,7 +38,7 @@ for(i=0;i<10;i++)
     sum = sum + a[i] - 1; 
 }
 
-/*
+
 if(sum>=70 && sum<80)
 {
     printf("\n\Heartrate unlikely to happen.");
@@ -57,10 +56,12 @@ else if(sum>100)
 {
     printf("\n\nSevere Heartrate Risk.");
 }
-*/
+
 
 printf("\n");
-
+    
+fout<<heartrate;
+    
 fout<<sum;
 fout<<"\n";
 
