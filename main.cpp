0b02e9ef77fb059bb42e4af8dada8c424041ef72
Saurabh Sinha <51476953+PrinceSs6312@users.noreply.github.com>
2019-08-21 15:17:32 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<fstream>
+#include"Heartrate.h"
+
+using namespace std;
+
+int main()
+{
+    system("clear");   
+    Heartrate();    
+    cout<<"\n";
+    display();
+
+    return 0;
+}
